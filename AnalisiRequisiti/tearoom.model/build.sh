a26bf5ba1a77604c8f1f74ce5dddd8a3fed3f3d7
Laura Mazzuca <lauramazzuca21@icloud.com>
2020-08-29 12:45:19 +0200

    testing features and adds build bash script

---
+++ AnalisiRequisiti/tearoom.model/build.sh
@@ -0,0 +1,3 @@
+gradle -Dorg.gradle.java.home=/Library/Java/JavaVirtualMachines/jdk1.8.0_171.jdk/Contents/Home/ -b build_ctxwaiter.gradle eclipse
+gradle -Dorg.gradle.java.home=/Library/Java/JavaVirtualMachines/jdk1.8.0_171.jdk/Contents/Home/ -b build_ctxclient.gradle eclipse
+gradle -Dorg.gradle.java.home=/Library/Java/JavaVirtualMachines/jdk1.8.0_171.jdk/Contents/Home/ -b build_ctxtearoom.gradle eclipse

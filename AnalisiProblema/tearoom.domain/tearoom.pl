b90489ad40bccd47f1ccfd910051be71fea8c279
Laura Mazzuca <lauramazzuca21@icloud.com>
2020-09-10 13:27:34 +0200

    updates tearoom.domain to working prototype

--- AnalisiProblema/tearoom.domain/tearoom.pl
+++ AnalisiProblema/tearoom.domain/tearoom.pl
@@ -1,10 +1,9 @@
 %====================================================================================
 % tearoom description   
 %====================================================================================
-mqttBroker("localhost", "1883", "unibo/mazzuca/basicrobot").
 context(ctxtearoom, "localhost",  "TCP", "8072").
- qactor( waiterwalker, ctxtearoom, "it.unibo.waiterwalker.Waiterwalker").
-  qactor( client, ctxtearoom, "it.unibo.client.Client").
+ qactor( client, ctxtearoom, "it.unibo.client.Client").
   qactor( waiter, ctxtearoom, "it.unibo.waiter.Waiter").
   qactor( smartbell, ctxtearoom, "it.unibo.smartbell.Smartbell").
   qactor( barman, ctxtearoom, "it.unibo.barman.Barman").
+  qactor( waiterwalker, ctxtearoom, "it.unibo.waiterwalker.Waiterwalker").

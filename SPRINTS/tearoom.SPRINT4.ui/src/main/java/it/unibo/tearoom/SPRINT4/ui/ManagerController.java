e0fbd53c25cb93cd54b24fee7fefc5324774a1ae
Nicholas Carroll <Carroll.Nicholas@protonmail.com>
2020-10-06 20:25:24 +0200

    adds misssing things in sprint4 folder

--- SPRINTS/tearoom.SPRINT4.ui/src/main/java/it/unibo/tearoom/SPRINT4/ui/ManagerController.java
+++ SPRINTS/tearoom.SPRINT4.ui/src/main/java/it/unibo/tearoom/SPRINT4/ui/ManagerController.java
@@ -1,19 +1,27 @@
 package it.unibo.tearoom.SPRINT4.ui;
 
+import org.eclipse.californium.core.CoapHandler;
+import org.eclipse.californium.core.CoapResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import connQak.actorQakCoapObserver;
 import connQak.configurator;
 import connQak.connQakCoap;
 import connQak.utils.ApplMessageUtils;
 import it.unibo.kactor.ApplMessage;
 import it.unibo.kactor.MsgUtil;
+import it.unibo.tearoom.SPRINT3.ui.ServerReply;
+import it.unibo.tearoom.SPRINT3.ui.WebSocketConfig;
 
 @Controller
 public class ManagerController {
-    String appName     		="tearoomGui";
+    String appName     		="managerTearoomGui";
     String viewModelRep		="startup";
     
     String robotHost = ""; //ConnConfig.hostAddr;		
@@ -26,20 +34,22 @@ public class ManagerController {
     connQakCoap waiterConn;
     connQakCoap barmanConn;
     
+    actorQakCoapObserver obs;
+    
 	public ManagerController() {
 	    configurator.configure();
 	    htmlPageMain  = configurator.getPageTemplate();
 	    robotHost =	configurator.getHostAddr();	
 	    robotPort = configurator.getPort();
 	
-	    smartbellConn = new connQakCoap(robotHost, robotPort, configurator.getQakdest(), configurator.getCtxqadest()  );  
+	    smartbellConn = new connQakCoap(robotHost, robotPort, "smartbell", configurator.getCtxqadest()  );  
 	    smartbellConn.createConnection();
  		
 	    waiterConn = new connQakCoap(robotHost, robotPort, "waiter", configurator.getCtxqadest()  );  
 	    waiterConn.createConnection();
 	    
-	    waiterConn = new connQakCoap(robotHost, robotPort, "barman", configurator.getCtxqadest()  );  
-	    waiterConn.createConnection();
+	    barmanConn = new connQakCoap(robotHost, robotPort, "barman", configurator.getCtxqadest()  );  
+	    barmanConn.createConnection();
 	      
 	 }
     
@@ -50,8 +60,9 @@ public class ManagerController {
 
 	
 	@MessageMapping("/update") 
-	@SendTo("/topic/display") 
+	@SendTo("/topic/display/manager") 
 	public ServerReply waiterInteraction(ClientRequest req) throws Exception {
+		preparePageUpdating();
 		ApplMessage barmanReply = getBarmanState();
 		ApplMessage	smartBellReply = getSmartbellState();
 		ApplMessage waiterReply = getWaiterState();
@@ -60,9 +71,10 @@ public class ManagerController {
 		String[] bellArgs = ApplMessageUtils.extractApplMessagePayloadArgs(smartBellReply);
 		String[] waiterArgs = ApplMessageUtils.extractApplMessagePayloadArgs(waiterReply);
 
-		//TO FIX: redir = ???
-		return new ServerReply("???????",barArgs[0], bellArgs[0], waiterArgs[0]);
+		//TO DO: redir = ?
+		return new ServerReply("redir?",barArgs[0], bellArgs[0], waiterArgs[0]);
 	}
+	
 	
 	private ApplMessage getBarmanState(){
  		ApplMessage getState = MsgUtil.buildRequest("web", "getstate", "getstate", "barman" );
@@ -81,5 +93,28 @@ public class ManagerController {
 		ApplMessage reply = waiterConn.request( getState );  
 		return reply;	
 	}
+	
+	
+	@Autowired
+	SimpMessagingTemplate simpMessagingTemplate;
+	
+	private void preparePageUpdating() {
+    	waiterConn.getClient().observe(new CoapHandler() { 
+			@Override
+			public void onLoad(CoapResponse response) {
+				if(response.getResponseText().contains("deliver-tea")) {
+					System.out.println("ClientController --> CoapClient changed -> " + response.getResponseText());
+					simpMessagingTemplate.convertAndSend(WebSocketConfig.topicForClient, 
+							new ServerReply("", "delivery"));
+				}
+			}
+
+			@Override
+			public void onError() {    
+				System.out.println("ClientController --> CoapClient error!");
+			}
+		});
+	}
+
 	
 }

7aef5da1c7261c92f797ae5b5379c613bd52dbcd
Nicholas Carroll <Carroll.Nicholas@protonmail.com>
2020-10-14 19:53:04 +0200

    formatting

--- SPRINTS/tearoom.SPRINT4/resources/json/SmartBellJson.kt
+++ SPRINTS/tearoom.SPRINT4/resources/json/SmartBellJson.kt
@@ -5,39 +5,39 @@ import org.json.JSONObject
 class smartBellJson : ActorJson {
 	//JsonObject is similar to a map
 	var jobj = JSONObject()
-	
+
 	init {
 		reset()
 	}
-	
-	fun setBusy(stat: Boolean){
+
+	fun setBusy(stat: Boolean) {
 		jobj.put("busy", stat)
 	}
-	
-	fun setClientArrived(stat: Boolean){
+
+	fun setClientArrived(stat: Boolean) {
 		jobj.put("ClientArrived", stat)
 	}
-	
-	fun setClientAccepted(stat: Int){
+
+	fun setClientAccepted(stat: Int) {
 		jobj.put("ClientAccepted", stat)
 	}
-	
-	fun setClientDenied(stat: Int){
+
+	fun setClientDenied(stat: Int) {
 		jobj.put("ClientDenied", stat)
 	}
-	
-	
+
+
 	//transforms the jsonObject in a json string string
-	override fun toJson():String{
+	override fun toJson(): String {
 		return jobj.toString()
 	}
-	
+
 	//resets to initial values the json object
-	override fun reset(){
+	override fun reset() {
 		jobj.put("busy", false)
 		jobj.put("ClientArrived", false)
 		jobj.put("ClientAccepted", -1)
 		jobj.put("ClientDenied", -1)
 	}
-	
+
 }

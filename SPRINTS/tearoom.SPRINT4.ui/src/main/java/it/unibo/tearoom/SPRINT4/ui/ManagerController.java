321c799573cd75f12bc2fdbf725a7707b1cda600
Giuseppe <smemo96@virgilio.it>
2020-10-14 16:07:01 +0200

    adds Json handler for CoAP messages in ManagerController

--- SPRINTS/tearoom.SPRINT4.ui/src/main/java/it/unibo/tearoom/SPRINT4/ui/ManagerController.java
+++ SPRINTS/tearoom.SPRINT4.ui/src/main/java/it/unibo/tearoom/SPRINT4/ui/ManagerController.java
@@ -3,12 +3,18 @@ package it.unibo.tearoom.SPRINT4.ui;
 import org.eclipse.californium.core.CoapHandler;
 import org.eclipse.californium.core.CoapResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jackson.JsonComponent;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import connQak.actorQakCoapObserver;
 import connQak.configurator;
@@ -21,128 +27,142 @@ import it.unibo.tearoom.SPRINT4.ui.WebSocketConfig;
 
 @Controller
 public class ManagerController {
-    String appName     		="managerTearoomGui";
-    String viewModelRep		="startup";
-    
-    String robotHost = ""; //ConnConfig.hostAddr;		
-    String robotPort = ""; //ConnConfig.port;
+	String appName = "managerTearoomGui";
+	String viewModelRep = "startup";
 
-    //DA CREARE FILE
-    String htmlPageMain  	= "manager-view-main";
-    
-    connQakCoap smartbellConn;   
-    connQakCoap waiterConn;
-    connQakCoap barmanConn;
-    
-    actorQakCoapObserver obs;
-    
+	String robotHost = ""; // ConnConfig.hostAddr;
+	String robotPort = ""; // ConnConfig.port;
+
+	// DA CREARE FILE
+	String htmlPageMain = "manager-view-main";
+
+	connQakCoap smartbellConn;
+	connQakCoap waiterConn;
+	connQakCoap barmanConn;
+
+	actorQakCoapObserver obs;
+
 	public ManagerController() {
-	    configurator.configure();
-	    htmlPageMain  = configurator.getPageTemplate();
-	    robotHost =	configurator.getHostAddr();	
-	    robotPort = configurator.getPort();
-	
-	    smartbellConn = new connQakCoap(robotHost, robotPort, "smartbell", configurator.getCtxqadest()  );  
-	    smartbellConn.createConnection();
- 		
-	    waiterConn = new connQakCoap(robotHost, robotPort, "waiter", configurator.getCtxqadest()  );  
-	    waiterConn.createConnection();
-	    
-	    barmanConn = new connQakCoap(robotHost, robotPort, "barman", configurator.getCtxqadest()  );  
-	    barmanConn.createConnection();
-	 }
-    
-	@GetMapping("manager/") 	 	 
+		configurator.configure();
+		htmlPageMain = configurator.getPageTemplate();
+		robotHost = configurator.getHostAddr();
+		robotPort = configurator.getPort();
+
+		smartbellConn = new connQakCoap(robotHost, robotPort, "smartbell", configurator.getCtxqadest());
+		smartbellConn.createConnection();
+
+		waiterConn = new connQakCoap(robotHost, robotPort, "waiter", configurator.getCtxqadest());
+		waiterConn.createConnection();
+
+		barmanConn = new connQakCoap(robotHost, robotPort, "barman", configurator.getCtxqadest());
+		barmanConn.createConnection();
+	}
+
+	@GetMapping("manager/")
 	public String entry(Model viewmodel) {
 		preparePageUpdating();
 		return htmlPageMain;
-	} 
+	}
 
-	
 	@Autowired
 	SimpMessagingTemplate simpMessagingTemplate;
-	
+
 	private void preparePageUpdating() {
-    	waiterConn.getClient().observe(new CoapHandler() { 
+		waiterConn.getClient().observe(new CoapHandler() {
 			@Override
 			public void onLoad(CoapResponse response) {
-				if(response.getResponseText().contains("listening")) {
+				ObjectMapper mapper = new ObjectMapper();
+				JsonNode msg = null;
+				try {
+					msg = mapper.readTree(response.getResponseText());
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+
+				if (response.getResponseText().contains("listening")) {
 					System.out.println("ClientController --> CoapClient changed -> " + response.getResponseText());
-					simpMessagingTemplate.convertAndSend(WebSocketConfig.topicForClient, 
+					simpMessagingTemplate.convertAndSend(WebSocketConfig.topicForClient,
 							new ServerReply("", "listening"));
-				}
-				else if(response.getResponseText().contains("Client_must_wait")) {
+				} else if (response.getResponseText().contains("Client_must_wait")) {
 					System.out.println("ClientController --> CoapClient changed -> " + response.getResponseText());
-					simpMessagingTemplate.convertAndSend(WebSocketConfig.topicForClient, 
+					simpMessagingTemplate.convertAndSend(WebSocketConfig.topicForClient,
 							new ServerReply("", "Client_must_wait"));
-				}
-				else if(response.getResponseText().contains("waiter_arrived")) {
+				} else if (response.getResponseText().contains("waiter_arrived")) {
 					System.out.println("ClientController --> CoapClient changed -> " + response.getResponseText());
-					simpMessagingTemplate.convertAndSend(WebSocketConfig.topicForClient, 
+					simpMessagingTemplate.convertAndSend(WebSocketConfig.topicForClient,
 							new ServerReply("", "waiter_arrived"));
-				}
-				else if(response.getResponseText().contains("waiter_rdy_leave")) {
+				} else if (response.getResponseText().contains("waiter_rdy_leave")) {
 					System.out.println("ClientController --> CoapClient changed -> " + response.getResponseText());
-					simpMessagingTemplate.convertAndSend(WebSocketConfig.topicForClient, 
+					simpMessagingTemplate.convertAndSend(WebSocketConfig.topicForClient,
 							new ServerReply("", "waiter_rdy_leave"));
-				}
-				else if(response.getResponseText().contains("waiter_rdy_getDrink")) {
+				} else if (response.getResponseText().contains("waiter_rdy_getDrink")) {
 					System.out.println("ClientController --> CoapClient changed -> " + response.getResponseText());
-					simpMessagingTemplate.convertAndSend(WebSocketConfig.topicForClient, 
+					simpMessagingTemplate.convertAndSend(WebSocketConfig.topicForClient,
 							new ServerReply("", "waiter_rdy_getDrink"));
-				}
-				else if(response.getResponseText().contains("deliver-tea-$CTABLE")) {
+				} else if (response.getResponseText().contains("deliver-tea-$CTABLE")) {
 					System.out.println("ClientController --> CoapClient changed -> " + response.getResponseText());
-					simpMessagingTemplate.convertAndSend(WebSocketConfig.topicForClient, 
+					simpMessagingTemplate.convertAndSend(WebSocketConfig.topicForClient,
 							new ServerReply("", "deliver-tea-$CTABLE"));
 				}
-			}		
+			}
+
 			@Override
-			public void onError() {    
+			public void onError() {
 				System.out.println("ClientController --> CoapClient error!");
 			}
 		});
-    	
-    	
-    	smartbellConn.getClient().observe(new CoapHandler() { 
+
+		smartbellConn.getClient().observe(new CoapHandler() {
 			@Override
 			public void onLoad(CoapResponse response) {
-				if(response.getResponseText().contains("Discard")) {
+				ObjectMapper mapper = new ObjectMapper();
+				JsonNode msg = null;
+				try {
+					msg = mapper.readTree(response.getResponseText());
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+				
+				if (response.getResponseText().contains("Discard")) {
 					System.out.println("ClientController --> CoapClient changed -> " + response.getResponseText());
-					simpMessagingTemplate.convertAndSend(WebSocketConfig.topicForClient, 
+					simpMessagingTemplate.convertAndSend(WebSocketConfig.topicForClient,
 							new ServerReply("", "Discard"));
-				}
-				else if(response.getResponseText().contains("Accept")) {
+				} else if (response.getResponseText().contains("Accept")) {
 					System.out.println("ClientController --> CoapClient changed -> " + response.getResponseText());
-					simpMessagingTemplate.convertAndSend(WebSocketConfig.topicForClient, 
-							new ServerReply("", "Accept"));
+					simpMessagingTemplate.convertAndSend(WebSocketConfig.topicForClient, new ServerReply("", "Accept"));
 				}
 			}
-			
+
 			@Override
-			public void onError() {    
+			public void onError() {
 				System.out.println("ClientController --> CoapClient error!");
 			}
 		});
-    	
-    	
-    	barmanConn.getClient().observe(new CoapHandler() { 
+
+		barmanConn.getClient().observe(new CoapHandler() {
 			@Override
 			public void onLoad(CoapResponse response) {
-				//STILL MISSING FROM QAK
-				if(response.getResponseText().contains("making-tea")) {
+				ObjectMapper mapper = new ObjectMapper();
+				JsonNode msg = null;
+				try {
+					msg = mapper.readTree(response.getResponseText());
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+				msg.get("order_ready");
+				// STILL MISSING FROM QAK
+				if (response.getResponseText().contains("making-tea")) {
 					System.out.println("ClientController --> CoapClient changed -> " + response.getResponseText());
-					simpMessagingTemplate.convertAndSend(WebSocketConfig.topicForClient, 
+					simpMessagingTemplate.convertAndSend(WebSocketConfig.topicForClient,
 							new ServerReply("", "making-tea"));
 				}
 			}
 
 			@Override
-			public void onError() {    
+			public void onError() {
 				System.out.println("ClientController --> CoapClient error!");
 			}
 		});
 	}
 
-	
 }

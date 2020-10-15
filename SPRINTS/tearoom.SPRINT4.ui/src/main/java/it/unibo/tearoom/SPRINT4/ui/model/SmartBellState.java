6da6b17e775427e94a2acd162915d928e7598766
Nicholas Carroll <Carroll.Nicholas@protonmail.com>
2020-10-15 12:28:14 +0200

    creates state in controller

--- SPRINTS/tearoom.SPRINT4.ui/src/main/java/it/unibo/tearoom/SPRINT4/ui/model/SmartBellState.java
+++ SPRINTS/tearoom.SPRINT4.ui/src/main/java/it/unibo/tearoom/SPRINT4/ui/model/SmartBellState.java
@@ -2,4 +2,53 @@ package it.unibo.tearoom.SPRINT4.ui.model;
 
 public class SmartBellState {
 
+	String clientsProcessed;
+	String clientsAdmitted;
+	String clientsWaiting;
+	String clientsWaitedLong;
+
+	public SmartBellState() {
+
+	}
+
+	public SmartBellState(String clientsProcessed, String clientsAdmitted, String clientsWaiting,
+			String clientsWaitedLong) {
+		this.clientsProcessed = clientsProcessed;
+		this.clientsAdmitted = clientsAdmitted;
+		this.clientsWaiting = clientsWaiting;
+		this.clientsWaitedLong = clientsWaitedLong;
+	}
+
+	public String getClientsProcessed() {
+		return clientsProcessed;
+	}
+
+	public void setClientsProcessed(String clientsProcessed) {
+		this.clientsProcessed = clientsProcessed;
+	}
+
+	public String getClientsAdmitted() {
+		return clientsAdmitted;
+	}
+
+	public void setClientsAdmitted(String clientsAdmitted) {
+		this.clientsAdmitted = clientsAdmitted;
+	}
+
+	public String getClientsWaiting() {
+		return clientsWaiting;
+	}
+
+	public void setClientsWaiting(String clientsWaiting) {
+		this.clientsWaiting = clientsWaiting;
+	}
+
+	public String getClientsWaitedLong() {
+		return clientsWaitedLong;
+	}
+
+	public void setClientsWaitedLong(String clientsWaitedLong) {
+		this.clientsWaitedLong = clientsWaitedLong;
+	}
+
 }

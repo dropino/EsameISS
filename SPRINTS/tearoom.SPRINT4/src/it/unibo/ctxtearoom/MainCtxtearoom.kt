ad0521bcce8c53cd7f10c7674f3c3d699da16c2e
Giuseppe <smemo96@virgilio.it>
2020-10-15 20:39:51 +0200

    removes log files

--- SPRINTS/tearoom.SPRINT4/src/it/unibo/ctxtearoom/MainCtxtearoom.kt
+++ SPRINTS/tearoom.SPRINT4/src/it/unibo/ctxtearoom/MainCtxtearoom.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
 	QakContext.createContexts(
-	        "localhost", this, "tearoomsys.pl", "sysRules.pl"
+	        "192.168.1.55", this, "clientsys.pl", "sysRules.pl"
 	)
 }
 

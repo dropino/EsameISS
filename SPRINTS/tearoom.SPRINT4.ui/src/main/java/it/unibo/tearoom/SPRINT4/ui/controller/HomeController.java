b84093da39980c57dd755c7c508c60561c04a6e8
Laura Mazzuca <lauramazzuca21@icloud.com>
2020-10-16 12:47:33 +0200

    adds homecontroller and homepage

---
+++ SPRINTS/tearoom.SPRINT4.ui/src/main/java/it/unibo/tearoom/SPRINT4/ui/controller/HomeController.java
@@ -0,0 +1,24 @@
+package it.unibo.tearoom.SPRINT4.ui.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	String htmlPageMain = "home-view-main";
+
+    
+	public HomeController() {
+
+	 }
+    
+	 @GetMapping("/") 	 	 
+	 public String entry(Model viewmodel) {
+	 	 return htmlPageMain;
+	 } 
+
+}
---
+++ SPRINTS/tearoom.SPRINT4.ui/src/main/resources/static/home-view.js
@@ -0,0 +1,12 @@
+
+$(document).on("click", "#btn-manager", function(event) {
+
+	window.location.assign(window.location.href + "/manager");
+
+});
+
+$(document).on("click", "#btn-client", function(event) {
+
+	window.location.assign(window.location.href + "/main");
+
+});

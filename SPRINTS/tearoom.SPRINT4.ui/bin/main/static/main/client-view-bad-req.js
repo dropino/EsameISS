9d548aa4b324b36fa63642b3c82d58ca011cb14d
Nicholas Carroll <Carroll.Nicholas@protonmail.com>
2020-10-20 17:09:18 +0200

    updates view

--- SPRINTS/tearoom.SPRINT4.ui/bin/main/static/main/client-view-bad-req.js
+++ SPRINTS/tearoom.SPRINT4.ui/bin/main/static/main/client-view-bad-req.js
@@ -5,6 +5,6 @@ var badtemp = urlParams.get('badtemp');
 if (timer == 'true') {
     $("#caption").text("Your timeout expired! We need to ask you to leave. Hope you will come back on a less busy day!");
 }
-else if (badtemp == 'true') {
+if (badtemp == 'true') {
 	$("#caption").text("Your temperature is too high. Come back when you're feeling better!");
 }
